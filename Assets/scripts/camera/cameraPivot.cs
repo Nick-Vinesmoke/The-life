94542b588578458978f80a3c8b81e9f8c2badb11
Nick Vinesmoke <123866627+Nick-Vinesmoke@users.noreply.github.com>
2023-03-21 22:09:05 +0200

    camera move

---
+++ Assets/scripts/camera/cameraPivot.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class cameraPivot : MonoBehaviour
+{
+    public Vector2 moveVector;
+    public float speed = 6f;
+    [SerializeField] Rigidbody2D rb;
+
+    void FixedUpdate()
+    {
+        moveVector.x = Input.GetAxisRaw("Horizontal");
+        moveVector.y = Input.GetAxisRaw("Vertical");
+        rb.velocity = new Vector2(moveVector.x * speed, moveVector.y * speed);
+    }
+}
