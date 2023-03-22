53e8017e0fb7f254d7cc63c520aa0f07ddecc23f
Nick Vinesmoke <123866627+Nick-Vinesmoke@users.noreply.github.com>
2023-03-22 09:33:23 +0200

    camera done

--- Assets/scripts/camera/cameraPivot.cs
+++ Assets/scripts/camera/cameraPivot.cs
@@ -1,15 +1,31 @@
 ﻿using UnityEngine;
+using Cinemachine;
 
 public class cameraPivot : MonoBehaviour
 {
     public Vector2 moveVector;
     public float speed = 6f;
     [SerializeField] Rigidbody2D rb;
+    public KeyCode zoom = KeyCode.Equals;
+    public KeyCode revert = KeyCode.Minus;
+    [SerializeField] Camera cam;
+    [SerializeField] CinemachineVirtualCamera virtualCamera;
 
     void FixedUpdate()
     {
         moveVector.x = Input.GetAxisRaw("Horizontal");
         moveVector.y = Input.GetAxisRaw("Vertical");
         rb.velocity = new Vector2(moveVector.x * speed, moveVector.y * speed);
+        if (Input.GetKey(zoom) && cam.orthographicSize < 60)
+        {
+            cam.orthographicSize = cam.orthographicSize + 0.1f;
+            virtualCamera.m_Lens.OrthographicSize = virtualCamera.m_Lens.OrthographicSize + 0.1f;
+            Debug.Log("+");
+        }else if (Input.GetKey(revert) && cam.orthographicSize> 1)
+        {
+            cam.orthographicSize = cam.orthographicSize - 0.1f;
+            virtualCamera.m_Lens.OrthographicSize = virtualCamera.m_Lens.OrthographicSize - 0.1f;
+            Debug.Log("-");
+        }
     }
 }
